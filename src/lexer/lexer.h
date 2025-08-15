6bb46e662245915745d2114bc940fa5315cee6c0
aiv-tmc <ilya_artemiev@icloud.com>
2025-08-15 13:03:07 +0300

    Add files via upload

---
+++ src/lexer/lexer.h
@@ -0,0 +1,168 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+// Macro to shift lexer position
+#define SHIFT(lexer, num) { \
+    lexer->position += (num); \
+    lexer->column += (num); \
+}
+
+// Macro to look ahead at characters
+#define NEXT(lexer, num) ((lexer)->position + (num) < (lexer)->length ? (lexer)->input[(lexer)->position + (num)] : '\0')
+
+// Macro to get character at position
+#define CHARACTER(lexer, num) (lexer->input[lexer->position + (num)])
+
+// Token types enumeration
+typedef enum {    
+    TOKEN_NUMBER,
+    TOKEN_CHAR,
+    TOKEN_NONE,
+    TOKEN_NULL,
+    TOKEN_STRING,
+    TOKEN_IF,
+    TOKEN_ELSE,
+    TOKEN_RETURN,
+    TOKEN_FREE,
+    TOKEN_BREAK,
+    TOKEN_CONTINUE,
+    TOKEN_BUILD,
+    TOKEN_SHARP,
+    TOKEN_PREPROC_MACRO,
+    TOKEN_PREPROC_ORG,
+    TOKEN_PREPROC_INCLUDE,
+    TOKEN_PREPROC_DEFINE,
+    TOKEN_PREPROC_ASSIGN,
+    TOKEN_PREPROC_UNDEF,
+    TOKEN_PREPROC_IFDEF,
+    TOKEN_PREPROC_IFNDEF,
+    TOKEN_PREPROC_ENDIF,
+    TOKEN_PREPROC_LINE,
+    TOKEN_PREPROC_ERROR,
+    TOKEN_PREPROC_PRAGMA,
+    TOKEN_OUTSIDE_BUILD,
+    TOKEN_OUTSIDE_CODE,
+    TOKEN_PLUS,
+    TOKEN_MINUS,
+    TOKEN_STAR,
+    TOKEN_SLASH,
+    TOKEN_PERCENT,
+    TOKEN_DOUBLE_STAR,
+    TOKEN_DOUBLE_STAR_EQ,
+    TOKEN_DOUBLE_AMPERSAND,
+    TOKEN_DOUBLE_PIPE,
+    TOKEN_BANG,
+    TOKEN_TILDE,
+    TOKEN_NE_TILDE,
+    TOKEN_PIPE,
+    TOKEN_AMPERSAND,
+    TOKEN_CARET,
+    TOKEN_SHR,
+    TOKEN_SHL,
+    TOKEN_SAR,
+    TOKEN_SAL,
+    TOKEN_ROR,
+    TOKEN_ROL,
+    TOKEN_SHR_EQ,
+    TOKEN_SHL_EQ,
+    TOKEN_SAR_EQ,
+    TOKEN_SAL_EQ,
+    TOKEN_ROR_EQ,
+    TOKEN_ROL_EQ,
+    TOKEN_AT,
+    TOKEN_GT,
+    TOKEN_LT,
+    TOKEN_GE,
+    TOKEN_LE,
+    TOKEN_DOUBLE_EQ,
+    TOKEN_NE,
+    TOKEN_EQUAL,
+    TOKEN_PLUS_EQ,
+    TOKEN_MINUS_EQ,
+    TOKEN_STAR_EQ,
+    TOKEN_SLASH_EQ,
+    TOKEN_PERCENT_EQ,
+    TOKEN_PIPE_EQ,
+    TOKEN_AMPERSAND_EQ,
+    TOKEN_CARET_EQ,
+    TOKEN_TILDE_EQ,
+    TOKEN_NE_TILDE_EQ,
+    TOKEN_DOUBLE_PLUS,
+    TOKEN_DOUBLE_MINUS,
+    TOKEN_DOLLAR,
+    TOKEN_COLON,
+    TOKEN_ELLIPSIS,
+    TOKEN_DOUBLE_DOT,
+    TOKEN_DOT,
+    TOKEN_DOUBLE_COLON,
+    TOKEN_COMMA,
+    TOKEN_SEMICOLON,
+    TOKEN_QUESTION,
+    TOKEN_UNDERSCORE,
+    TOKEN_DOUBLE_UNDERSCORE,
+    TOKEN_LCURLY,
+    TOKEN_RCURLY,
+    TOKEN_LBRACKET,
+    TOKEN_RBRACKET,
+    TOKEN_LBRACE,
+    TOKEN_RBRACE,
+    TOKEN_LPAREN,
+    TOKEN_RPAREN,
+    TOKEN_MODIFIER,
+    TOKEN_SIZE,
+    TOKEN_PARSE,
+    TOKEN_REALLOC,
+    TOKEN_ALLOC,
+    TOKEN_TYPE,
+    TOKEN_SELF,
+    TOKEN_PUBLIC,
+    TOKEN_INVIS,
+    TOKEN_ID,
+    TOKEN_STACK,
+    TOKEN_PUSH,
+    TOKEN_POP,
+    TOKEN_SYSCALL,
+    TOKEN_EOF,
+    TOKEN_ERROR
+} TokenType;
+
+// Token structure
+typedef struct {
+    TokenType type;
+    char* value;
+    int line;
+    int column;
+    int length;
+} Token;
+
+// Token names array
+extern const char* token_names[];
+
+// Lexer structure
+typedef struct {
+    const char* input;
+    int length;
+    int position;
+    int line;
+    int column;
+    Token* tokens;
+    int token_count;
+    int token_capacity;
+} Lexer;
+
+// Function prototypes
+Lexer* init_lexer(const char* input);
+void free_lexer(Lexer* lexer);
+void tokenize(Lexer* lexer);
+Token* read_tokens_from_file(const char* filename, int* token_count);
+void free_tokens(Token* tokens, int token_count);
+
+#endif
+

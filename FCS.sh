523c722f88ba3b27ee72b93cfacc1c498ea71abe
aiv-tmc <ilya_artemiev@icloud.com>
2025-09-07 16:31:12 +0300

    Update FCS.sh

--- FCS.sh
+++ FCS.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-gcc src/lexer/lexer.c src/parser/parser.c src/error_manager/error_manager.c src/main.c -o paxsi
-sudo cp paxsi /usr/local/bin
+gcc src/lexer/lexer.c src/parser/parser.c src/error_manager/error_manager.c src/main.c -o paxsi && cp paxsi /usr/local/bin

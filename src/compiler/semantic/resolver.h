7505a5a0e2cc9ea7422abbf4f97516f121619d92
AIV <aiv-tmc@proton.me>
2026-08-03 10:06:25 +0300

    Delete src directory

--- src/compiler/semantic/resolver.h
+++
@@ -1,9 +0,0 @@
-#ifndef RESOLVER_H
-#define RESOLVER_H
-
-#include "symbol.h"
-
-/* The resolver is provided by the driver via a function pointer.
-   This file is only for declaration. */
-
-#endif
